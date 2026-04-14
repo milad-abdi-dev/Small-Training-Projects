632b8ebf8b604aca1ae3a68c9770f2ee0c4d5612
Milad <miladabdi1374@gmail.com>
2026-04-14 14:45:15 +0200

    Init: Add a probject and also new changes on it. This project is only for training some C#, .NET, ASP.NET Core or EF Concepts

---
+++ VehicleManagementSystem/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using VehicleManagementSystem;
+
+List<Vehicle> vehicles = new List<Vehicle>();
+vehicles.Add(new Car());
+vehicles.Add(new Bike());
+vehicles.Add(new Bike());
+vehicles.Add(new Car());
+
+foreach (Vehicle vehicle in vehicles)
+{
+    vehicle.CalculateFuelConsumption();
+}
---
+++ VehicleManagementSystem/Vehicle.cs
@@ -0,0 +1,24 @@
+﻿namespace VehicleManagementSystem;
+
+public abstract class Vehicle
+{
+    public abstract double CalculateFuelConsumption();
+}
+
+public class Car : Vehicle
+{
+    public override double CalculateFuelConsumption()
+    {
+        Console.WriteLine("Car fuel consumption.");
+        return 20;
+    }
+}
+
+public class Bike : Vehicle
+{
+    public override double CalculateFuelConsumption()
+    {
+        Console.WriteLine("Bike fuel consumption.");
+        return 0;
+    }
+}
